ed7f1d76ffa2fa63206ec884f820a06c09bf17f5
Ethan Buchman <ethan@coinculture.info>
2015-01-09 03:14:01 -0500

    begin overhaul. back to one script per request

--- cmd/lllc-server/main.go
+++ cmd/lllc-server/main.go
@@ -1,53 +1,50 @@
 package main
 
 import (
-    "os"
-    "flag"
-    "fmt"
-    "strconv"
-    "encoding/hex"
-    "github.com/project-douglas/lllc-server"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"github.com/project-douglas/lllc-server"
+	"os"
+	"strconv"
 )
 
-
-
 // simple lllc-server and cli
-func main(){
-    client := flag.Bool("c", false, "specify files to compile, separated by space. this must come last")
-    host := flag.String("h", "", "specify the host and port")
-    localOnly := flag.Bool("local", false, "only listen internally")
-    port := flag.Int("port", 9999, "listen port")
-    nonet := flag.Bool("no-net", false, "do you have lll locally?")
+func main() {
+	client := flag.Bool("c", false, "specify files to compile, separated by space. this must come last")
+	host := flag.String("h", "", "specify the host and port")
+	localOnly := flag.Bool("local", false, "only listen internally")
+	port := flag.Int("port", 9999, "listen port")
+	nonet := flag.Bool("no-net", false, "do you have lll locally?")
 
-    flag.Parse()
+	flag.Parse()
 
+	if *host != "" {
+		lllcserver.URL = *host + "/" + "compile"
+		fmt.Println("url:", lllcserver.URL)
+	}
 
-    if *host != ""{
-        lllcserver.URL = *host+"/"+"compile"
-        fmt.Println("url:", lllcserver.URL)
-    }
-
-    if *client{
-        lllcserver.CheckMakeDir(lllcserver.TMP)
-        tocompile := flag.Args()
-        fmt.Println("to compile:", tocompile)
-        if *nonet{
-            b, err := lllcserver.CompileLLLWrapper(tocompile[0])
-            if err != nil{
-                fmt.Println("failed to compile!", err)
-                os.Exit(0)
-            }
-            fmt.Println("bytecode:", hex.EncodeToString(b))
-        } else{
-            lllcserver.RunClient(tocompile, false) // these are all files (literal is false)
-        }
-    }else {
-        lllcserver.CheckMakeDir(lllcserver.ServerTmp)
-        addr := ""
-        if *localOnly{
-            addr = "localhost"
-        }
-        addr += ":"+strconv.Itoa(*port)
-        lllcserver.StartServer(addr)
-    }
+	if *client {
+		lllcserver.CheckMakeDir(lllcserver.TMP)
+		tocompile := flag.Args()[0]
+		fmt.Println("to compile:", tocompile)
+		if *nonet {
+			b, err := lllcserver.CompileLLLWrapper(tocompile)
+			if err != nil {
+				fmt.Println("failed to compile!", err)
+				os.Exit(0)
+			}
+			fmt.Println("bytecode:", hex.EncodeToString(b))
+		} else {
+			lllcserver.RunClient(tocompile, false) // these are all files (literal is false)
+		}
+	} else {
+		lllcserver.CheckMakeDir(lllcserver.ServerTmp)
+		addr := ""
+		if *localOnly {
+			addr = "localhost"
+		}
+		addr += ":" + strconv.Itoa(*port)
+		lllcserver.StartServer(addr)
+	}
 }

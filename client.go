868de2fe1e8dcfb2c9334e009816971a2abdbed7
Ethan Buchman <ethan@coinculture.info>
2015-03-12 22:16:17 -0400

    print server addr

--- client.go
+++ client.go
@@ -31,7 +31,7 @@ func resolveCode(filename string, literal bool) (code []byte, err error) {
 // send compile request to server or compile directly
 func (c *CompileClient) compileRequest(req *Request) (respJ *Response, err error) {
 	if c.config.Net {
-		logger.Warnln("compiling remotely...")
+		logger.Warnln("compiling remotely...", c.config.URL)
 		respJ, err = requestResponse(req)
 	} else {
 		logger.Warnln("compiling locally...")
